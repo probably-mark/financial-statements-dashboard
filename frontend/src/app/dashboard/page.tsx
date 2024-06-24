4538b0d29c1da13d1079c0c2482ba317da2bd256
Mark McAvoy <mark.mcavoy@senzai.ai>
2024-06-23 21:24:19 -0400

    initial hero design

---
+++ frontend/src/app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+import Hero from "@/app/daisy-components/Hero";
+
+export default function Dashboard() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between">
+      <div>Dashboard</div>
+    </main>
+  );
+}
